9abb33198fe159f2064829b9ae5b9b3a1c65e585
RagulDhanabalan2904 <raguldhanabalan2904@gmail.com>
2024-07-09 20:48:56 +0530

    Controller class added in route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
 // User Authentication Routes
-Route::get('/register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->middleware('guest')->name('register');
-Route::post('/register', 'App\Http\Controllers\Auth\RegisterController@register');
-Route::get('/login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login')->name('login');
-Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
-Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->middleware('guest')->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // protected
 Route::middleware(['auth'])->group(function () {
